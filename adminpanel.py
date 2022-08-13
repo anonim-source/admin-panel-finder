392626910078bda7bfd84d42831b2faaa4af6c00
ANKA949 <99740525+ANKA949@users.noreply.github.com>
2022-08-13 11:17:23 +0300

    Update adminpanel.py

--- adminpanel.py
+++ adminpanel.py
@@ -1,23 +1,31 @@
 import requests
+from pyfiglet import Figlet
+from colorama import *
+from fake_useragent import UserAgent
 
-link = input("hedef link girin :")
+# Başlık
+f = Figlet(font='slant')
+print(Fore.LIGHTMAGENTA_EX + f.renderText('Admin Finder'))
 
-dosya = open("links.txt","r+")
+link = input(Fore.LIGHTGREEN_EX+"hedef link girin örnek(ornek.com) :")
+
+dosya = open("links.txt","r")
 dosya_r = dosya.readlines()
 dosya.close()
-
+fake = UserAgent()
+header = {'User-Agent': fake.random}
 for  i in dosya_r:
     try:
-        r = requests.get(link + "/" + i)
+        r = requests.get(link + "/" + i, headers=header)
         c = r.url
         a = r.status_code
         if a == 200:
             print("-"*30)
-            print("admin panel bulundu >",i)
-            print("link >",c)
+            print(Fore.LIGHTRED_EX+"admin panel bulundu >",i)
+            print(Fore.LIGHTYELLOW_EX+"link >",c)
             print("-"*30)
         else:
-            print("bulunamadı >",i)
+            print(Fore.LIGHTWHITE_EX+"bulunamadı >",i)
             
     except:
         print("sitemden cıkıldı ")
